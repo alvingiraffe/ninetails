88a26fcb5c5b25e29e4d327a32d567b9dddd2802
Alvin Richardson <arichardson@ojolabs.com>
2022-07-29 19:28:10 -0500

    add node descriptions

--- src/app/api/question.ts
+++ src/app/api/question.ts
@@ -107,7 +107,7 @@ const nodes: Node[] = [
     ] ,
     canBeCompleted: true,
     currentPath: ['Financial Readiness'],
-    description: "something about financial readiness and how we're about to start asking more.",
+    description: "Financial readiness is the first step towards buying a home. Lenders are looking for a few key things to approve your application - let’s walk through them together!",
     parentIds: [],
     isEnabled() {
       return true;
@@ -131,7 +131,7 @@ const nodes: Node[] = [
     ] ,
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Credit Score'],
-    description: "something about credit score",
+    description: "Your credit score is one of the first thing lenders look at to determine how much they are willing to lend and one of the most important in deciding what interest rate you'll pay.",
     parentIds: ['financial_readiness'],
     isEnabled() {
       return true;
@@ -155,7 +155,9 @@ const nodes: Node[] = [
     ] ,
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Credit Education'],
-    description: "what is credit and stuff",
+    description: `Your credit score is an important piece of the homebuying puzzle. Lenders use credit scores as one factor to decide who to lend to, so it is important to know what your credit score is. 
+    There are three main companies that track consumer credit: Experian, Equifax, and TransUnion. Every year, you can request a free copy of your credit report from each bureau to see your score as well as your credit account history, any credit inquiries, and relevant public records.
+    In addition to the credit bureaus, your bank may offer free credit monitoring and websites like CreditKarma and CreditSesame can help you stay on top of your credit throughout the year.`,
     parentIds: ['financial_readiness'],
     isEnabled() {
       return questionsAndAnswers['your_credit_score'] == undefined || questionsAndAnswers['your_credit_score'] == 'dont_know'
@@ -169,7 +171,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Credit Repair'],
-    description: "placeholder",
+    description: `While your credit might not be lender-ready today, there are plenty of ways to get it back on track. Improving your credit score can save you thousands, if not tens of thousands, of dollars in helping you qualify for a better interest rate. Working with a credit repair service can help you jumpstart this process. 1`,
     parentIds: ['financial_readiness'],
     isEnabled() {
       return questionsAndAnswers['your_credit_score'] == undefined || questionsAndAnswers['your_credit_score'] == 'low';
@@ -183,7 +185,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Saving for a Downpayment'],
-    description: "placeholder",
+    description: `Whether you're putting down 20% or 3%, you may be able to get approved for multiple lending options.`,
     parentIds: ['financial_readiness'],
     isEnabled() {
       return true;
@@ -197,7 +199,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'First time buyer'],
-    description: "placeholder",
+    description: `A first time home buyer is someone who has either never owned a home or hasn't owned their primary residence in the past three years. `,
     parentIds: ['financial_readiness'],
     isEnabled() {
       return true;
@@ -211,7 +213,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Veteran'],
-    description: "placeholder",
+    description: `Preferrential loan terms are one way the US Government thanks veterans and active duty service members for their service.`,
     parentIds: ['financial_readiness'],
     isEnabled() {
       return true;
@@ -225,7 +227,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financing'],
-    description: "placeholder",
+    description: `While it's tempting to start looking for homes, the first step in the homebuying purchase should usually getting your financing in order. Getting pre-approved will help you set a budget, communicate with your agent, and define search, so it is a great first step towards purchasing a home. `,
     parentIds: [],
     isEnabled() {
       return true;
@@ -239,7 +241,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financing','Check for USDA Eligibility'],
-    description: "placeholder",
+    description: `Where are you looking to buy? Government-backed loans are availble for buyers looking in less-densely populated areas. `,
     parentIds: ['financing'],
     isEnabled() {
       return true;
@@ -253,7 +255,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Financing','USDA loans'],
-    description: "placeholder",
+    description: `Great news! It looks like you might qualify for a USDA loan. These government-backed loans are offered by many lenders and secured by the US Government, allowing you to put no money down and potentially qualify for a lower interest rate than you may receive from a conventional lender. Maximum household limits and specific location restrictions may apply. USDA loans also require Private Mortgage Insurance for the duration of the loan.`,
     parentIds: ['financing'],
     isEnabled() {
       return (questionsAndAnswers['your_credit_score'] == undefined || questionsAndAnswers['your_credit_score'] == 'medium') && 
@@ -262,14 +264,14 @@ const nodes: Node[] = [
     },
   },
   {
-    id: 'va_recommendation',
+    id: 'veteran_financing',
     label: 'VA Loans',
     question: undefined,
     articles: [
     ],
     canBeCompleted: true,
     currentPath: ['Financing','VA loans'],
-    description: "placeholder",
+    description: `Great news! It looks like you might qualify for a VA loan. These government-backed loans are offered by many lenders and provide advantageous terms to veterans, active duty service members, and their spouses. In addition to lower interest rates than most conventional loans, VA loans allow you to purchase with no downpayment and may reduce your total closing costs, allowing you to get into your dream home sooner. One main restriction is that property's must conform to the VA's Minimum Property Standards, so unless you are financing a purchase and improvement, look for move-in ready homes. `,
     parentIds: ['financing'],
     isEnabled() {
       return questionsAndAnswers['is_veteran'] == undefined || questionsAndAnswers['is_veteran'] == 'yes';
@@ -284,7 +286,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Searching for homes'],
-    description: "placeholder",
+    description: `Now that you know your financing options, it's time to start searching for homes. Working with a registered Realtor will help you learn more about the neighborhoods you're interested in, write a compelling offer, and win the house of your dreams.`,
     parentIds: [],
     isEnabled() {
       return true;
@@ -298,7 +300,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Negotiating an Offer'],
-    description: "placeholder",
+    description: `It may be tempting to offer a little more money or waive an inspection in the hopes that the seller will accept your offer, but it's important to go into any negotiation with hard limits on what you are (and are not!) willing to compromise on. `,
     parentIds: [],
     isEnabled() {
       return true;
@@ -312,7 +314,7 @@ const nodes: Node[] = [
     ],
     canBeCompleted: true,
     currentPath: ['Closing'],
-    description: "placeholder",
+    description: `Closing can be a simultaneously exciting and boring time. The best thing you can do while you're under contract is to respond promptly to any questions from your lender, try to be patient, and not make any large purchases. While it may be tempting to go furniture shopping for your new home, any large purchases or new lines of credit could cause you to lose your financing. It's best to stick to window-shopping until you have keys in hand. `,
     parentIds: [],
     isEnabled() {
       return true;
@@ -323,15 +325,10 @@ const nodes: Node[] = [
     label: 'Home Ownership',
     question: undefined,
     articles: [
-      {
-        url: 'https://www.google.com/search?q=How+much+should+I+have+for+a+down+payment%3F&oq=How+much+should+I+have+for+a+down+payment%3F',
-        label: "How much should I have for a down payment?",
-        snippet: "here's a snippet for downpayment",
-      },
     ] ,
     canBeCompleted: false,
     currentPath: ['Home Ownership', 'Credit Education'],
-    description: "All about owning a home",
+    description: `Congratulations! The years of saving, months of looking, and weeks of closing have finally paid off! `,
     parentIds: [],
     isEnabled() {
       return true
