dd9c03a42831d3ea84c069899246501168de60a4
Alvin Richardson <arichardson@ojolabs.com>
2022-07-29 21:06:50 -0500

    added articles

--- src/app/api/question.ts
+++ src/app/api/question.ts
@@ -95,16 +95,6 @@ const nodes: Node[] = [
     label: 'Financial Readiness',
     question: questions[0],
     articles: [
-      {
-        url: 'https://www.google.com/search?q=What%27s+a+credit+score%3F&oq=What%27s+a+credit+score%3F',
-        label: "What's a credit score?",
-        snippet: "Here's a snippet for credit score.",
-      },
-      {
-        url: 'https://www.google.com/search?q=What%27s+a+credit+score%3F&oq=What%27s+a+credit+score%3F',
-        label: "No but seriously, what is it?",
-        snippet: "Alright fine, some more information.",
-      },
     ] ,
     canBeCompleted: true,
     currentPath: ['Financial Readiness'],
@@ -121,13 +111,13 @@ const nodes: Node[] = [
     articles: [
       {
         url: 'https://www.google.com/search?q=What%27s+a+credit+score%3F&oq=What%27s+a+credit+score%3F',
-        label: "What's a credit score?",
-        snippet: "here's a snippet for credit score",
+        label: "What is good credit and why is it important?",
+        snippet: "Credit scores can range from 300 to 850, with the average score in the high-600s. Having a lower score can make it harder to qualify for financing…",
       },
       {
         url: 'https://www.google.com/search?q=What%27s+a+credit+score%3F&oq=What%27s+a+credit+score%3F',
-        label: "No but seriously, what is it?",
-        snippet: "Alright fine, some more information.",
+        label: "Equifax, Experian, & TransUnion - which score matters?",
+        snippet: "Your scores from each credit bureau might be different. Each bureau uses a proprietary calculation, which leads to the discrepancies. You should only worry….",
       },
     ] ,
     canBeCompleted: true,
@@ -144,14 +134,19 @@ const nodes: Node[] = [
     question: undefined,
     articles: [
       {
-        url: 'https://www.google.com/search?q=How+much+should+I+have+for+a+down+payment%3F&oq=How+much+should+I+have+for+a+down+payment%3F',
-        label: "How much should I have for a down payment?",
-        snippet: "here's a snippet for downpayment",
+        url: 'https://www.google.com',
+        label: "How can I check my credit scores?",
+        snippet: "It’s easy to check your credit scores with all three credit bureaus. As a consumer, you have the right to view your credit report once a year at each…",
       },
       {
-        url: 'https://www.google.com/search?q=What%27s+a+credit+score%3F&oq=What%27s+a+credit+score%3F',
-        label: "No but seriously, what is it?",
-        snippet: "Alright fine, some more information.",
+        url: 'https://www.google.com',
+        label: "5 steps to dispute an error on your credit report",
+        snippet: "Whether it’s an account you don’t recognize or a past-due credit card notification, errors on your credit report can have a big impact on your score…",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Why do my credit scores differ among the bureaus?",
+        snippet: "You might have three (or more!) different credit scores, depending on who you ask. A big difference between scores might be an indication that something….",
       },
     ] ,
     canBeCompleted: true,
@@ -169,6 +164,16 @@ const nodes: Node[] = [
     label: 'Credit Repair',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "Start repairing your credit with Ovation today!",
+        snippet: "Ovation offers one-on-one coaching to: negotiate directly with credit bureaus on your behalf, provide guidance on how to grow...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "What is good credit and why is it important?",
+        snippet: "Credit scores can range from 300 to 850, with the average score in the high-600s. Having a lower score can make it harder to qualify for financing…",
+      },
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Credit Repair'],
@@ -183,6 +188,16 @@ const nodes: Node[] = [
     label: 'Saving for a Downpayment',
     question: questions[2],
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "'I need a 20% downpayment' and other myths about mortgages",
+        snippet: "You may have heard you need a downpayment equal to 20% of the purchase price of your new home. While a 20% downpayment allows…",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "What is PMI and why should I care?",
+        snippet: "If you intend on putting less than 20% down on your new home purchase, you may be surprised to learn that you owe Private Mortgage Insurance. This doesn’t insure…",
+      },
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'Saving for a Downpayment'],
@@ -197,6 +212,21 @@ const nodes: Node[] = [
     label: 'First time buyer',
     question: questions[3],
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "Who is a first time homeowner?",
+        snippet: "You may be surprised to learn a first-time homeowner may have previously owned their own…",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Owned a home 4 years ago? Why your lender might consider you a ‘first time’ home buyer",
+        snippet: "Lenders define a first time homeowner as someone who has not owned their principal residence…",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Your first home might be closer than you think",
+        snippet: "Did you know their are special programs to make homes more affordable to first time homeowners…",
+      },
     ],
     canBeCompleted: true,
     currentPath: ['Financial Readiness', 'First time buyer'],
@@ -208,12 +238,22 @@ const nodes: Node[] = [
   },
   {
     id: 'veteran',
-    label: 'Veteran',
+    label: 'Veteran Status',
     question: questions[4],
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "Criteria for Veteran status for Federal Aid purposes",
+        snippet: "At least 90 days of active-duty service including at least 30 consecutive days (your DD214 must…",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Guard and Reserve members receive ‘Veteran’ status",
+        snippet: "A recently signed law gives veteran status to National Guard members who served 20 years…",
+      },
     ],
     canBeCompleted: true,
-    currentPath: ['Financial Readiness', 'Veteran'],
+    currentPath: ['Financial Readiness', 'Veteran Status'],
     description: `Preferrential loan terms are one way the US Government thanks veterans and active duty service members for their service.`,
     parentIds: ['financial_readiness'],
     isEnabled() {
@@ -236,12 +276,27 @@ const nodes: Node[] = [
   },
   {
     id: 'usda_eligibility',
-    label: 'Check for USDA Eligibility',
+    label: 'USDA Eligibility',
     question: questions[6],
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "What's rural? Surprising areas the USDA categorizes as 'Rural'",
+        snippet: "Rural areas might be closer than you think. While we think of USDA loans as being eligible for farmland...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "How to qualify for a USDA loan when buying in the suburbs",
+        snippet: "Your white picket fence in the suburbs may qualify you for a loan from the US Department of Agriculture...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Property eligibility - USDA.gov",
+        snippet: "Final determination of property eligibility must be made by Rural Development upon receipt of a completed…",
+      },
     ],
     canBeCompleted: true,
-    currentPath: ['Financing','Check for USDA Eligibility'],
+    currentPath: ['Financing','USDA Eligibility'],
     description: `Where are you looking to buy? Government-backed loans are availble for buyers looking in less-densely populated areas. `,
     parentIds: ['financing'],
     isEnabled() {
@@ -253,9 +308,19 @@ const nodes: Node[] = [
     label: 'USDA Loans',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "Get Pre-Approved with Guaranteed Rate! ",
+        snippet: "The US Government offers a number of mortgage products that are less restrictive and more affordable than conventional loans...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Top 5 USDA Home Loan Lenders ",
+        snippet: "5 best USDA Loan lenders, compared & reviewed. Updated rates for July, 2022. Are you looking for USDA Home Loan lenders...",
+      },
     ],
     canBeCompleted: true,
-    currentPath: ['Financing','USDA loans'],
+    currentPath: ['Financing','USDA Loans'],
     description: `Great news! It looks like you might qualify for a USDA loan. These government-backed loans are offered by many lenders and secured by the US Government, allowing you to put no money down and potentially qualify for a lower interest rate than you may receive from a conventional lender. Maximum household limits and specific location restrictions may apply. USDA loans also require Private Mortgage Insurance for the duration of the loan.`,
     parentIds: ['financing'],
     isEnabled() {
@@ -269,9 +334,19 @@ const nodes: Node[] = [
     label: 'VA Loans',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "Get Pre-Approved with Veterans United! ",
+        snippet: "Trusted VA Loan Lender of 300,000+ Veterans Nationwide. Get Your VA Loan! VA Loan Expertise & Personal Service...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Your Complete Guide to the VA Home Loan",
+        snippet: "A VA home loan (also known as a Department of Veterans Affairs home loan) is one of the most useful military benefits. If you....",
+      },
     ],
     canBeCompleted: true,
-    currentPath: ['Financing','VA loans'],
+    currentPath: ['Financing','VA Loans'],
     description: `Great news! It looks like you might qualify for a VA loan. These government-backed loans are offered by many lenders and provide advantageous terms to veterans, active duty service members, and their spouses. In addition to lower interest rates than most conventional loans, VA loans allow you to purchase with no downpayment and may reduce your total closing costs, allowing you to get into your dream home sooner. One main restriction is that property's must conform to the VA's Minimum Property Standards, so unless you are financing a purchase and improvement, look for move-in ready homes. `,
     parentIds: ['financing'],
     isEnabled() {
@@ -281,12 +356,27 @@ const nodes: Node[] = [
 
   {
     id: 'searching_for_homes',
-    label: 'Searching for homes',
+    label: 'Searching for Homes',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "What to look for when buying a house",
+        snippet: "When deciding what to look for in a house, focus in on things that can't be easily changed. Location, backyard, garage, and foundation...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "'Cozy' and 'Great Potential' - How to decipher real estate eupheisms ",
+        snippet: "Ask a real estate agent to describe a tiny house and they'll invariably use the word 'cozy:' we're breaking down the biggest euphemisms in real estate...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Property Experts Reveal the Top Home Buyer Mistakes",
+        snippet: "From buying a house for the decor to ignoring the crack in the foundation, property experts reveal the top home buyer mistakes they see...",
+      },
     ],
     canBeCompleted: true,
-    currentPath: ['Searching for homes'],
+    currentPath: ['Searching for Homes'],
     description: `Now that you know your financing options, it's time to start searching for homes. Working with a registered Realtor will help you learn more about the neighborhoods you're interested in, write a compelling offer, and win the house of your dreams.`,
     parentIds: [],
     isEnabled() {
@@ -298,6 +388,16 @@ const nodes: Node[] = [
     label: 'Negotiating an Offer',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "8 Simple Rules for Negotiating Your Offer and Winning the House",
+        snippet: "Negotiating a home purchase price can be intimidating, especially for first-time buyers. Start by deciding what you're not willing to compromise on....",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "How to Put in an Offer on a Home that's Overpriced",
+        snippet: "With interest rates rising and prices falling, you may encounter sellers who are reluctant to face reality and lower their price...",
+      },
     ],
     canBeCompleted: true,
     currentPath: ['Negotiating an Offer'],
@@ -312,6 +412,16 @@ const nodes: Node[] = [
     label: 'Closing',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "3 Things to Avoid Before You Close (and 2 To Do!)",
+        snippet: "Your offer was accepted, so now what? It can be frustrating to feel like you have nothing to do, but don't let that frustration torpedo your home purchase!...",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "So your offer was accepted, now what?",
+        snippet: "Whether you made 1 offer or 12, it is an exhilerating feeling to finally have an offer accepted! What happens next might be a lot of hurry up and wait. ",
+      },
     ],
     canBeCompleted: true,
     currentPath: ['Closing'],
@@ -326,6 +436,21 @@ const nodes: Node[] = [
     label: 'Home Ownership',
     question: undefined,
     articles: [
+      {
+        url: 'https://www.google.com',
+        label: "How to manage your biggest asset",
+        snippet: "",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Have a Designer Decorate One Room in Your New Home",
+        snippet: "",
+      },
+      {
+        url: 'https://www.google.com',
+        label: "Air filters, termites, and appliances, oh my! ",
+        snippet: "Take the guess work out of home maintenance and repairs with Digs, by OJO. ",
+      },
     ] ,
     canBeCompleted: false,
     currentPath: ['Home Ownership'],
